fdee2b2eb005699589bc5f36d7048310e51331b8
Arafat Hossain Rifat <95178439+arafat-rifat@users.noreply.github.com>
2023-12-06 12:43:44 +0600

    fetching data and implement on design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,8 +25,10 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
